b911f0263226538b27f44835c0e652a19109bdad
mttrys <mttrys@gmail.com>
2017-06-18 10:23:17 -0700

    Initial framework setup

---
+++ server.js
@@ -0,0 +1,33 @@
+// FileReader
+// Express
+// Node
+// MySQL
+// Google Maps API
+
+
+// FileReader
+  // With each file
+    // Insert data into MySQL
+
+
+// Express
+  // GET
+    // REQUEST from client
+      // Day: 0-6
+      // Time: 0-24
+
+      // MySQL
+        // QUERY Database
+
+      // QUERY Results
+        // Google Maps API
+        // Stats
+
+
+    // RESPONSE to client
+      // QUERY Results
+        // Google Maps API
+        // Stats
+
+
+
